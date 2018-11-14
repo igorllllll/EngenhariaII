8e9e076afba53b63e835137618a4299a87cd53ab
mpgxc <mateuspgx5@live.com>
2018-11-14 01:32:37 -0300

    new

--- src/Bean/Livro.java
+++ src/Bean/Livro.java
@@ -14,9 +14,21 @@ public class Livro {
     private String _titulo;
     private String _autor;
     private String _genero;
-    private int _ano;
-    private float _valor;
+    private String _ano;
+    private String _valor;
 
+    public Livro() {
+    }
+
+    public Livro(String _titulo, String _autor, String _genero, String _ano, String _valor) {
+        this._titulo = _titulo;
+        this._autor = _autor;
+        this._genero = _genero;
+        this._ano = _ano;
+        this._valor = _valor;
+    }
+
+    
     public String getTitulo() {
         return _titulo;
     }
@@ -41,19 +53,19 @@ public class Livro {
         this._genero = _genero;
     }
 
-    public int getAno() {
+    public String getAno() {
         return _ano;
     }
 
-    public void setAno(int _ano) {
+    public void setAno(String _ano) {
         this._ano = _ano;
     }
 
-    public float getValor() {
+    public String getValor() {
         return _valor;
     }
 
-    public void setValor(float _valor) {
+    public void setValor(String _valor) {
         this._valor = _valor;
     }
 }
