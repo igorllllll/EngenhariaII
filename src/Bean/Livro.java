c574d5e88853c25ac27c866527fa3c644318ead7
mpgxc <mateuspgx5@live.com>
2018-11-13 23:59:07 -0300

    get|set's

--- src/Bean/Livro.java
+++ src/Bean/Livro.java
@@ -15,4 +15,44 @@ public class Livro {
     private String _genero;
     private int _ano;
     private float _valor;
+
+    public String getTitulo() {
+        return _titulo;
+    }
+
+    public void setTitulo(String _titulo) {
+        this._titulo = _titulo;
+    }
+
+    public String getAutor() {
+        return _autor;
+    }
+
+    public void setAutor(String _autor) {
+        this._autor = _autor;
+    }
+
+    public String getGenero() {
+        return _genero;
+    }
+
+    public void setGenero(String _genero) {
+        this._genero = _genero;
+    }
+
+    public int getAno() {
+        return _ano;
+    }
+
+    public void setAno(int _ano) {
+        this._ano = _ano;
+    }
+
+    public float getValor() {
+        return _valor;
+    }
+
+    public void setValor(float _valor) {
+        this._valor = _valor;
+    }
 }
